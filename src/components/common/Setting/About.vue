1859b16d22d1a7b4a87f4393b3de42b2fa69fe91
xywlxywl <835384839@qq.com>
2023-03-29 18:32:14 +0800

    Update About.vue

--- src/components/common/Setting/About.vue
+++ src/components/common/Setting/About.vue
@@ -52,7 +52,17 @@ onMounted(() => {
           超级一休，让你轻松享受AI科技带来的智能体验！不仅可以帮你快速准确写出论文、律师函、直播话术、作文、代码，还可以秒写演讲稿、工作报告、入职申请等，让你的工作变得更加轻松高效！
 					它将彻底改变我们的生活习惯，让你的生活更加轻松，让你的工作变得更加高效！让我们一起感受超级一休给我们带来的新鲜体验吧！
       </p>
-			</div>
-      </div>
+ </div>
+      <p>{{ $t("setting.api") }}：{{ config?.apiModel ?? '-' }}</p>
+      <p v-if="isChatGPTAPI">
+        {{ $t("setting.balance") }}：{{ config?.balance ?? '-' }}
+      </p>
+      <p v-if="!isChatGPTAPI">
+        {{ $t("setting.reverseProxy") }}：{{ config?.reverseProxy ?? '-' }}
+      </p>
+      <p>{{ $t("setting.timeout") }}：{{ config?.timeoutMs ?? '-' }}</p>
+      <p>{{ $t("setting.socks") }}：{{ config?.socksProxy ?? '-' }}</p>
+      <p>{{ $t("setting.httpsProxy") }}：{{ config?.httpsProxy ?? '-' }}</p>
+    </div>
   </NSpin>
 </template>
