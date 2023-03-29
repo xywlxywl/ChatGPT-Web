20cc4c348b6cd368ef080d0929d965e70a3a2a57
xywlxywl <835384839@qq.com>
2023-03-29 19:09:01 +0800

    Update About.vue

--- src/components/common/Setting/About.vue
+++ src/components/common/Setting/About.vue
@@ -40,7 +40,8 @@ onMounted(() => {
   <NSpin :show="loading">
     <div class="p-4 space-y-4">
       <h2 class="text-xl font-bold">
-        Version - {{ pkg.version }}
+        客服微信： wxid_4ta1x11bv57v12
+	
       </h2>
       <div class="p-2 space-y-2 rounded-md bg-neutral-100 dark:bg-neutral-700">
         <p>
